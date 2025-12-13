e3d1ab6fae0da7327df5f26473f57945877e50a0
erin1401 <iindra0406@gmail.com>
2025-12-13 15:31:29 +0700

    Add files via upload

---
+++ manifest-jason.js
@@ -0,0 +1,21 @@
+{
+  "name": "Juragan Buah POS",
+  "short_name": "JuraganBuah",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#1abc9c",
+  "theme_color": "#1abc9c",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "assets/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "assets/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
